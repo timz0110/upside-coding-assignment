bf668b1cf8842254acb9fb24cc825666f990fde5
treynolds <tim.reynolds@concur.com>
2018-08-15 21:11:47 -0400

    First commit to initialize project

---
+++ cypress/integration/examples/test_bbc.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+
+describe('Upside - My First Cypress Test', function() {
+  it('Visits the bbc.com', function() {
+    cy.visit('https://bbc.com')
+
+    cy.contains('Sport').click()
+
+    // Should be on a new URL which includes '/sport'
+      cy.url().should('include', '/sport')
+
+      cy.get('#top-stories')
+        .find('a')
+        .first()
+        .click()
+        .then(function($a){
+             // extract the fully qualified href property
+             const href = $a.prop('href')
+
+             cy.url().should('include', href)
+        }
+        )
+  })
+})
